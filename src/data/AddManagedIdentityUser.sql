df9370c0bed323d184105716a0416678854c6407
Leif Larsen <me@leiflarsen.org>
2025-03-19 10:57:10 +0100

    vector search api

--- src/data/AddManagedIdentityUser.sql
+++ src/data/AddManagedIdentityUser.sql
@@ -1,4 +1,4 @@
-CREATE USER TechExcelMI FROM EXTERNAL PROVIDER;
-ALTER ROLE db_datareader ADD MEMBER TechExcelMI;
-ALTER ROLE db_datawriter ADD MEMBER TechExcelMI;
+CREATE USER id-oy57sn5lm4mbu FROM EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER id-oy57sn5lm4mbu;
+ALTER ROLE db_datawriter ADD MEMBER id-oy57sn5lm4mbu;
 
